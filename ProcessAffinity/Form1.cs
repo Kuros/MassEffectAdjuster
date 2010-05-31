8c5b5e4c8102842d98ed088df19e80cd5cd0bc27
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2010-05-31 14:18:34 +0300

    Initial commit

---
+++ ProcessAffinity/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessAffinity
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		
+		}
+
+		
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string name = "FlashBuilder";
+
+			
+			try
+			{
+				Process process = Process.GetProcessesByName(name).Single();
+				ShakeAffinity(process);
+			}
+			catch
+			{
+			}
+			
+		}
+
+		private static void ShakeAffinity(Process process)
+		{
+			IntPtr affinity, systemAffinity;
+			ProcessAPI.GetProcessAffinityMask(process.Handle, out affinity, out systemAffinity);
+			IntPtr newAffinity = (IntPtr)(affinity.ToInt64() ^ 2);
+			ProcessAPI.SetProcessAffinityMask(process.Handle, newAffinity);
+			Thread.Sleep(500);
+			ProcessAPI.SetProcessAffinityMask(process.Handle, affinity);
+		}
+
+		
+	}
+}
---
+++ ProcessAffinity/ProcessAPI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ProcessAffinity
+{
+	public static class ProcessAPI
+	{
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern Boolean SetProcessAffinityMask(IntPtr hProcess, IntPtr dwProcessAffinityMask);
+		
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern bool GetProcessAffinityMask(IntPtr hProcess, out IntPtr lpProcessAffinityMask, out IntPtr lpSystemAffinityMask);
+
+		public static void ShakeAffinity(Process process)
+		{
+			IntPtr affinity, systemAffinity;
+			ProcessAPI.GetProcessAffinityMask(process.Handle, out affinity, out systemAffinity);
+			IntPtr newAffinity = (IntPtr)(affinity.ToInt64() ^ 2);
+			ProcessAPI.SetProcessAffinityMask(process.Handle, newAffinity);
+			Thread.Sleep(500);
+			ProcessAPI.SetProcessAffinityMask(process.Handle, affinity);
+		}
+	}
+}
